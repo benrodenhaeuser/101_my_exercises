5539d9bfa99a551420053fcca0e10acd7df612f6
Ben Rodenhäuser <ben@karo.berlin>
2017-05-03 23:37:03 +0200

    improved version of longest_palindrome

--- string/longest_palindrome.rb
+++ string/longest_palindrome.rb
@@ -1,59 +1,57 @@
-=begin
+# Find a longest palindromic substring of a given string
 
-Problem statement: Find the longest palindromic substring of a given string
+# Notice that there may be several maximal-length palindromic substrings of a given string. The problem description does not prescribe a way of dealing with such a situation. The task is simply to find *some* maximal palindromic substring.
 
-- Method that takes a string as argument, and returns a string, with no side effects
-- There is not necessarily a unique longest palindromic substring. So we will return
-*all* the longest ones.
-- Algorithm:
-  - By recursion
-  - If given string is palindrome, return [string]
-  - Elsif longest palindromes of "string without first char"
-    are longer than longest palindromes of "string without last char"
-    then return longest palindromes string without first char
-  - Elsif longest palindromes of string without last char are longer then return those
-  - Elsif they have same length, return concatenation after removing duplicates
+# there are solutions in linear time, but that's not what we are aiming for here (see https://en.wikipedia.org/wiki/Longest_palindromic_substring).
 
-Pseudocode:
+# solution 1 (brute force recursion)
 
-def palindromes(string)
-  if reverse(string) == string
-    return [string]
-  else
-    if palindromes(string without first character) are longer than
-      palindromes(string without last character)
-      then return palindromes(string without first character)
-    elsif palindromes(string without last) are longer
-      then return those
-    elseif they have same length
-      then return concatenation, after removing duplicates
-    end
-end
-
-=end
-
-def palindromes(string)
+def longest_palindrome(string)
   if string.reverse == string
-    [string]
+    string
   else
     right_string = string.slice(1..string.size - 1)
     left_string = string.slice(0..string.size - 2)
 
-    palindromes_right = palindromes(right_string)
-    palindromes_left = palindromes(left_string)
+    palindrome_right = longest_palindrome(right_string)
+    palindrome_left = longest_palindrome(left_string)
 
-    palindromes_right_size = palindromes_right.first.size
-    palindromes_left_size = palindromes_left.first.size
-
-    if palindromes_right_size > palindromes_left_size
-      palindromes_right
-    elsif palindromes_right_size < palindromes_left_size
-      palindromes_left
+    if palindrome_left.size >= palindrome_right.size
+      palindrome_left
     else
-      (palindromes_left + palindromes_right).uniq
+      palindrome_right
     end
   end
 end
 
-p palindromes("annabrznnzghynny") # ["anna", "znnz", "ynny"]
-p palindromes('aogishgsaoifsjannaalium') # ['anna'], close to tractability border ...
+# solution 2 (recursion with memoization)
+
+# "In computing, memoization or memoisation is an optimization technique used primarily to speed up computer programs by storing the results of expensive function calls and returning the cached result when the same inputs occur again." (https://en.wikipedia.org/wiki/Memoization)
+
+# (the following code runs a lot faster than solution 1 above)
+
+def longest_palindrome(string, l_p = {})
+  if string.reverse == string
+    l_p[string] == string
+    string
+  else
+    right_str = string[0..-2]
+    left_str = string[1..-1]
+
+    # the next two lines are responsible for the efficiency increase:
+    l_p[right_str] = longest_palindrome(right_str, l_p) unless l_p[right_str]
+    l_p[left_str] = longest_palindrome(left_str, l_p) unless l_p[left_str]
+
+    if l_p[right_str].length >= l_p[left_str].length
+      l_p[right_str]
+    else
+      l_p[left_str]
+    end
+  end
+end
+
+p longest_palindrome('anna') # 'anna'
+p longest_palindrome("annabrznnzghynny") # 'anna'
+p longest_palindrome('aogishgsaoifsjannaalium') # 'anna'
+p longest_palindrome('bananas') # 'anana'
+p longest_palindrome('abracadabra') # 'aca'
