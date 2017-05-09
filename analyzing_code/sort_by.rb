4321a4f7217181838affbf18173dcc0df0f3d4eb
Ben Rodenhäuser <ben@karo.berlin>
2017-05-10 01:56:46 +0200

    sort_by

--- analyzing_code/sort_by.rb
+++ analyzing_code/sort_by.rb
@@ -16,11 +16,17 @@ end
 arr.sort_by(&:to_i)
 # => ['0', '3', '10']
 
-# One way to think of what sort_by is doing is like this: It (1) maps the array to an array of pairs, (2) sorts that array by the second component of each pair (relying on `<=>`), and (3) then projects each pair onto its first component.
+# One way to think of what sort_by is doing is like this:
+# It (1) maps the array to an array of pairs,
+# (2) sorts that array by the second component of each pair (relying on `<=>`),
+# and (3) then projects each pair onto its first component.
 
-# sort_by makes life very easy for us because all we have to is what the "mapping" in step 1 should look like. To specify this, we can either pass a block or a method symbol to sort_by, as seen above.
+# sort_by makes life very easy for us because all we have to is specify what
+# the "mapping" in step 1 should look like. To achieve this, we can either pass
+# a block or a method symbol to sort_by, as seen above.
 
-# We can actually mimick the three steps in Ruby code as follows. For our running example, this looks like this:
+# We can actually mimick these three steps in Ruby code as follows. For our
+# running example, this looks like this:
 
 arr.map { |elem| [elem, elem.to_i] } # step (1)
   .sort { |pair1, pair2| pair1.last <=> pair2.last } # step (2)
@@ -28,7 +34,10 @@ arr.map { |elem| [elem, elem.to_i] } # step (1)
 
 # => ['0', '3', '10']
 
-# We can even implement our own sort_by functionality in this way:
+# This makes it conceptually clearer (for me) how sort_by is connected with
+# `sort` and `<=>`.
+
+# Based on these observations, we can implement our own sort_by functionality:
 
 def my_sort_by(collection, method_symbol)
   collection.map { |elem| [elem, elem.send(method_symbol)] }
