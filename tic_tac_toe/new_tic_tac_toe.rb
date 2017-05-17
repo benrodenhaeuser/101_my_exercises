3be9c5e164fdd42037e3c28db5de35f55b7cad07
Ben Rodenhäuser <ben@karo.berlin>
2017-05-18 01:11:11 +0200

    different variants of negamax algorithm

--- tic_tac_toe/new_tic_tac_toe.rb
+++ tic_tac_toe/new_tic_tac_toe.rb
@@ -1,5 +1,7 @@
 # tic tac toe
 
+require 'benchmark'
+
 PLAYERS = ['X', 'O']
 INITIAL = ('-' * 9).chars
 WIN_LINES = [
@@ -28,18 +30,20 @@ def payoff(player, state)
   winner?(player, state) ? 1 : (winner?(opponent(player), state) ? -1 : 0)
 end
 
-def actions(player, state)
-  actions = []
-  (0..8).select { |index| state[index] == '-' }.each do |index|
-    state[index] = player
-    actions << state.dup
-    state[index] = '-'
+def empty_slots(state)
+  (0..8).select { |index| state[index] == '-' }
+end
+
+def choices(player, state)
+  empty_slots(state).map do |index|
+    choice = state.dup
+    choice[index] = player
+    choice
   end
-  actions
 end
 
 def get_choice(player, state)
-  actions(player, state).sample
+  choices(player, state).sample # here, we would like to include negamax choices
 end
 
 def get_game(state = INITIAL)
@@ -52,12 +56,79 @@ def get_game(state = INITIAL)
   state
 end
 
-def display(state)
-  state.each_slice(3) { |slice| puts slice.join(' ').center(10) }
-  puts "> payoff: #{payoff('X', state)}\n\n"
+def mirror(state)
+  state.dup.map do |value|
+    case value
+    when 'X' then 'O'
+    when 'O' then 'X'
+    else
+      '-'
+    end
+  end
 end
 
 # testing things
+
+# def display(state)
+#   state.each_slice(3) { |slice| puts slice.join(' ').center(10) }
+#   puts "> payoff: #{payoff('X', state)}\n\n"
+# end
+#
 # 10.times do
 #   display(get_game())
 # end
+
+# brute force negamax
+
+def get_value(player = 'X', state = INITIAL)
+  if terminal?(state)
+    payoff(player, state)
+  else
+    choices(player, state).map do |choice|
+      -get_value(opponent(player), choice)
+    end.max
+  end
+end
+
+# puts Benchmark.realtime { get_value }
+# 6.14 seconds
+
+# negamax with memoization
+
+def get_value(player = 'X', state = INITIAL, memo = {})
+  return memo[state] if memo[state]
+
+  if terminal?(state)
+    memo[state] = payoff(player, state)
+  else
+    values = choices(player, state).map do |choice|
+      -get_value(opponent(player), choice, memo)
+    end
+    memo[state] = values.max
+  end
+end
+
+# puts Benchmark.realtime { get_value }
+# # 0.19 seconds
+
+# negamax with memoization and symmetries
+
+def get_value(player = 'X', state = INITIAL, memo = {})
+  if memo[state]
+    return memo[state]
+  elsif memo[mirror(state)]
+    return -memo[mirror(state)]
+  end
+
+  if terminal?(state)
+    memo[state] = payoff(player, state)
+  else
+    values = choices(player, state).map do |choice|
+      -get_value(opponent(player), choice, memo)
+    end
+    memo[state] = values.max
+  end
+end
+
+# puts Benchmark.realtime { get_value }
+# # 0.11 seconds
