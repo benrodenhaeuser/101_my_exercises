bae2c42dfd3152e1af779b6a417d2391e8fb5d35
Ben Rodenhäuser <ben@karo.berlin>
2017-06-14 17:05:44 +0200

    n queens

--- matrix/n_queens.rb
+++ matrix/n_queens.rb
@@ -2,17 +2,20 @@
 
 require 'benchmark'
 
-SIZE = 10
+SIZE = 8
 
 # ----------------------------------------------------------------------------
 # SOLUTION 1
 # ----------------------------------------------------------------------------
 
-# using a nested array representing the board as a data structure
+# data structure: matrix, i.e., a nested array represents the chess board
 
 # approach:
-# we model the problem by considering a sequence of placement choices, i.e., each choice consists in placing a queen on the chess board.
-# rather than considering random such choices, we only try choices that are locally consistent in the sense that they we do not place a queen in a square where it would attack another queen.
+#   - only carry out locally viable moves, taking into account which squares on
+#     the board are already covered by some queen.
+#   - a solution is obtained once SIZE queens are placed on the board.
+
+# very complicated!
 
 OPEN_SQUARE = 0
 QUEEN_SQUARE = 1
@@ -33,68 +36,35 @@ def open_column?(board, col_idx)
   board.all? { |row| row[col_idx] == OPEN_SQUARE }
 end
 
-def open_upwards_diag?(board, row_idx, col_idx)
-  up_diag_slots = get_up_diag_slots(board, row_idx, col_idx)
-  up_diag_slots.all? { |row_idx, col_idx| board[row_idx][col_idx] == OPEN_SQUARE }
+def open_diff_diag?(board, row_idx, col_idx)
+  (0...SIZE).all? do |row|
+    (0...SIZE).all? do |col|
+      if row - col == row_idx - col_idx
+        board[row][col] == OPEN_SQUARE
+      else
+        true
+      end
+    end
+  end
 end
 
-def open_downwards_diag?(board, row_idx, col_idx)
-  down_diag_slots = get_down_diag_slots(board, row_idx, col_idx)
-  down_diag_slots.all? { |row_idx, col_idx| board[row_idx][col_idx] == OPEN_SQUARE }
+def open_sum_diag?(board, row_idx, col_idx)
+  (0...SIZE).all? do |row|
+    (0...SIZE).all? do |col|
+      if row + col == row_idx + col_idx
+        board[row][col] == OPEN_SQUARE
+      else
+        true
+      end
+    end
+  end
 end
 
-def get_up_diag_slots(board, row_idx, col_idx)
-  up_diag_slots = [[row_idx, col_idx]]
-  stored_row_idx = row_idx
-  stored_col_idx = col_idx
-
-  while row_idx > 0 && col_idx < (SIZE - 1)
-    row_idx -= 1
-    col_idx += 1
-    up_diag_slots << [row_idx, col_idx]
-  end
-
-  row_idx = stored_row_idx
-  col_idx = stored_col_idx
-
-  while row_idx < (SIZE - 1) && col_idx > 0
-    row_idx += 1
-    col_idx -= 1
-    up_diag_slots << [row_idx, col_idx]
-  end
-
-  up_diag_slots
-end
-
-def get_down_diag_slots(board, row_idx, col_idx)
-  down_diag_slots = [[row_idx, col_idx]]
-
-  stored_row_idx = row_idx
-  stored_col_idx = col_idx
-
-  while row_idx > 0 && col_idx > 0
-    row_idx -= 1
-    col_idx -= 1
-    down_diag_slots << [row_idx, col_idx]
-  end
-
-  row_idx = stored_row_idx
-  col_idx = stored_col_idx
-
-  while row_idx < (SIZE - 1) && col_idx < (SIZE - 1)
-    row_idx += 1
-    col_idx += 1
-    down_diag_slots << [row_idx, col_idx]
-  end
-
-  down_diag_slots
-end
-
-def available_slots(board)
+def slots(board)
   slots = []
   (0...SIZE).each do |row_idx|
     (0...SIZE).each do |col_idx|
-      slots << [row_idx, col_idx] if board[row_idx][col_idx] == OPEN_SQUARE
+      slots << [row_idx, col_idx]
     end
   end
   slots
@@ -103,12 +73,12 @@ end
 def unattacked?(board, row_idx, col_idx)
   open_row?(board, row_idx) &&
   open_column?(board, col_idx) &&
-  open_upwards_diag?(board, row_idx, col_idx) &&
-  open_downwards_diag?(board, row_idx, col_idx)
+  open_diff_diag?(board, row_idx, col_idx) &&
+  open_sum_diag?(board, row_idx, col_idx)
 end
 
 def unattacked_slots(board)
-  available_slots(board).select do |row_idx, col_idx|
+  slots(board).select do |row_idx, col_idx|
     unattacked?(board, row_idx, col_idx)
   end
 end
@@ -134,24 +104,58 @@ def solve1(board = initialize_board, number_of_choices = 0, solutions = [])
   solutions
 end
 
+# board = initialize_board
+# p unattacked_slots(board)
+# p available_slots(board)
+# p open_diff_diag?(board, 1, 1)
+# p open_sum_diag?(board, 1, 1)
+
+
 # ----------------------------------------------------------------------------
 # SOLUTION 2:
 # ----------------------------------------------------------------------------
 
-# use a flat array to represent the queens
+# solution 1 is very complex. this seems to be due to the choice of data structure.
+
+# here, we explore another option: represent the problem by means of a flat array recording the positioning of the queens. every queen corresponds to one array element.
 
 # a queen's row is given by the array index
 # a queen's column is given by the value at the array index
 
-# this changes the representation of the constraints:
+# this changes the representation of the problem constraints:
 
-# row is open <=> automatically satisfied
-# col is open <=> "no element of array has value of col"
-# diagonals: combination of index and value (sum diagonal/diff diagonal)
+# row not covered <=> automatically satisfied
+# col not covered <=> choose number not yet contained in array
+# diagonals not covered <=> check using sums/differences of index and value
+
+def non_covered_col?(queens, new_queen)
+  queens.all? do |queen|
+    queen != new_queen
+  end
+end
+
+def non_covered_diff_diag?(queens, new_queen)
+  queens.all? do |queen|
+    queen - queens.index(queen) != new_queen - queens.size
+  end
+end
+
+def non_covered_sum_diag?(queens, new_queen)
+  queens.all? do |queen|
+    queen + queens.index(queen) != new_queen + queens.size
+  end
+end
+
+def viable_choices2(queens)
+  (0...SIZE).select do |choice|
+    non_covered_col?(queens, choice) && non_covered_diff_diag?(queens, choice) &&
+    non_covered_sum_diag?(queens, choice)
+  end
+end
 
 def solve2(queens = [], solutions = [])
   return solutions << queens.inspect if queens.size == SIZE
-  constrained_choices(queens).each do |choice|
+  viable_choices2(queens).each do |choice|
     queens << choice
     solve2(queens, solutions)
     queens.pop
@@ -159,121 +163,143 @@ def solve2(queens = [], solutions = [])
   solutions
 end
 
-def distinct_col?(queens, new_queen)
-  queens.all? do |queen|
-    queen != new_queen
-  end
-end
-
-def distinct_diff_diag?(queens, new_queen)
-  queens.all? do |queen|
-    queen - queens.index(queen) != new_queen - queens.size
-  end
-end
-
-def distinct_sum_diag?(queens, new_queen)
-  queens.all? do |queen|
-    queen + queens.index(queen) != new_queen + queens.size
-  end
-end
-
-def constrained_choices(queens)
-  (0...SIZE).select do |choice|
-    distinct_col?(queens, choice) && distinct_diff_diag?(queens, choice) &&
-    distinct_sum_diag?(queens, choice)
-  end
-end
-
-
 # ----------------------------------------------------------------------------
 # SOLUTION 3: refactor solution 2
 # ----------------------------------------------------------------------------
 
-# use an attack? method to model the problem more clearly
+# this improves on solution 2 by making it more readable.
+# the idea is to pull out the negation.
+# instead of negations all over the place, we just have it in one place:
+#
+# a new queen is a viable choice if it does not attack any of the queens
+# already on the board.
+#
+# also, we iterate over the array of queens placed so far only once instead of # three times (on each occasion)
 
-# TODO: note that the index gives the row and the value at the index gives the column. our terminology does not properly reflect this, I think, i.e., sometimes the terminology suggests the indices are the queens, sometimes the terminology suggests the values are the queens.
-
-def attack?(queens, queen1, queen2)
-  unless queen1 == queen2 # same index, i.e., same queen
-    queens[queen1] == queens[queen2] || # same col
-    queens[queen1] + queen1 == queens[queen2] + queen2 || # same sum diag
-    queens[queen1] - queen1 == queens[queen2] - queen2 # same diff diag
-  end
-end
-
-def get_choices
-  choice_hash = {}
-  (0...SIZE).each do |choice|
-    choice_hash[choice] = true
-  end
-  choice_hash
-end
-
-def good_choice?(queens)
-  (0...queens.size).all? { |queen| !attack?(queens, queen, queens.size - 1) }
-end
-
-def solve3(queens = [], choices = get_choices, solutions = [])
-  return solutions << queens.inspect if queens.size == SIZE
-  choices.select { |choice, val| val == true }.each do |choice, _|
-    queens << choice
-    choices[choice] = false
-    solve3(queens, choices, solutions) if good_choice?(queens)
-    queens.pop
-    choices[choice] = true
-  end
-  solutions
-end
-
-# ----------------------------------------------------------------------------
-# SOLUTION 4:
-# ----------------------------------------------------------------------------
-
-# test diagonals only in the end
-
-def attack?(queens, queen1, queen2)
-  unless queen1 == queen2 # same index, i.e., same queen
-    queens[queen1] == queens[queen2] || # same col
-    queens[queen1] + queen1 == queens[queen2] + queen2 || # same sum diag
-    queens[queen1] - queen1 == queens[queen2] - queen2 # same diff diag
-  end
-end
-
-def solve4(queens = [], choices = get_choices, solutions = [])
-  return solutions << queens.inspect if queens.size == SIZE && valid?(queens)
-  choices.select { |choice, val| val == true }.each do |choice, _|
-    queens << choice
-    choices[choice] = false
-    solve4(queens, choices, solutions)
-    queens.pop
-    choices[choice] = true
-  end
-  solutions
-end
-
-def get_choices
-  choice_hash = {}
-  (0...SIZE).each do |choice|
-    choice_hash[choice] = true
-  end
-  choice_hash
-end
-
-def valid?(queens)
-  (0...SIZE).all? do |queen1|
-    (0...SIZE).all? do |queen2|
-      !attack?(queens, queen1, queen2)
+def solve3(queens = [], solutions = [])
+  if queens.size == SIZE
+    solutions << queens.inspect
+  else
+    viable_choices3(queens).each do |new_queen|
+      queens << new_queen
+      solve3(queens, solutions)
+      queens.pop
     end
   end
+  solutions
+end
+
+def attack3?(queens, new_queen)
+  queens.any? do |queen|
+    queen == new_queen ||
+    queen - queens.index(queen) == new_queen - queens.size ||
+    queen + queens.index(queen) == new_queen + queens.size
+  end
+end
+
+def viable_choices3(queens)
+  (0...SIZE).select do |new_queen|
+    !attack3?(queens, new_queen)
+  end
 end
 
 # ----------------------------------------------------------------------------
+# SOLUTION 4: memoize covered squares
+# ----------------------------------------------------------------------------
 
-# puts Benchmark.realtime { p solve1.size }
-# ^ 1.901837000157684 for a single solution!
-# puts Benchmark.realtime { p solve2.size } # 0.018800999831408262
-puts Benchmark.realtime { p solve3.size } # 0.010900000110268593 for 8x8
-# puts Benchmark.realtime { p solve4.size } # 0.23814400006085634 for 8x8
+# to avoid having to compute non-attacks all the time, we memoize the currently
+# covered columns and diagonals in a hash
 
-# presumably, a lot of effort here goes into the attack? method.
-# dijkstra discusses a way to memoize covered fields, something I had been thinking about initially, but couldn't find a good method to do it.
+def solve4(queens = [], covered = init_covered, solutions = [])
+  if queens.size == SIZE
+    solutions << queens.inspect
+  else
+    # p viable_choices(covered, queens.size)
+    # p covered
+    viable_choices(covered, queens.size).each do |new_queen|
+      add_to_covered(covered, queens.size, new_queen)
+      queens << new_queen
+      solve4(queens, covered, solutions)
+      queens.pop
+      remove_from_covered(covered, queens.size, new_queen)
+    end
+  end
+  solutions
+end
+
+def viable_choices(covered, queen_row)
+  (0...SIZE).select do |queen_col|
+    covered[:cols][queen_col] == 0 &&
+    covered[:sum_diags][queen_row + queen_col] == 0 &&
+    covered[:diff_diags][queen_row - queen_col] == 0
+  end
+end
+
+def init_covered
+  cols = (0..SIZE - 1).map { |col| [col, 0] }.to_h
+  sum_diags = (0..2*(SIZE - 1)).map { |diag| [diag, 0] }.to_h
+  diff_diags = (-(SIZE - 1)..SIZE - 1).map { |diag| [diag, 0] }.to_h
+
+  {
+    cols: cols,
+    sum_diags: sum_diags,
+    diff_diags: diff_diags
+  }
+end
+
+def add_to_covered(covered, queen_row, queen_col)
+  covered[:cols][queen_col] += 1
+  covered[:sum_diags][queen_row + queen_col] += 1
+  covered[:diff_diags][queen_row - queen_col] += 1
+end
+
+def remove_from_covered(covered, queen_row, queen_col)
+  covered[:cols][queen_col] -= 1
+  covered[:sum_diags][queen_row + queen_col] -= 1
+  covered[:diff_diags][queen_row - queen_col] -= 1
+end
+
+# ----------------------------------------------------------------------------
+# FURTHER OPTIONS:
+# ----------------------------------------------------------------------------
+
+# more strategies:
+#   - symmetry breaking
+#   - iterative repair (start with something, massage it to a solution)
+#   - genetic programming
+
+# find a single solution in linear time
+
+# ----------------------------------------------------------------------------
+# TESTS:
+# ----------------------------------------------------------------------------
+
+
+# 8x8 board:
+puts Benchmark.realtime { p solve1.size } # 0.01819799980148673
+# puts Benchmark.realtime { p solve2.size } # 0.01819799980148673
+# puts Benchmark.realtime { p solve3.size } # 0.016749999951571226
+# puts Benchmark.realtime { p solve4.size } # 0.009391000028699636
+
+# 8x8 board:
+# solve2: 0.01819799980148673
+# solve3: 0.016749999951571226
+# solve4: 0.009391000028699636
+
+# 10x10 board:
+# solve2: 0.39056000020354986
+# solve3: 0.29880600003525615
+# solve4: 0.0982429999858141
+
+# 12x12 board:
+# solve2: 14.65682000014931
+# solve3: 10.745362000074238
+# solve4:  2.664822000078857
+
+# 14x14 board:
+# solve2: 642.9831059998833
+# solve3: 480.1802369998768
+# solve4:  94.39550500037149
+
+# so memoization has a quite dramatic effect on performance.
+# solution 3 improves on solution 2 mainly because the number of times we walk through the queens array is tripled, which is inefficient.
--- matrix/square_matrix_symmetries.rb
+++ matrix/square_matrix_symmetries.rb
@@ -2,9 +2,10 @@ require 'benchmark'
 
 =begin
 - A tic tac toe board is a n x n matrix.
-- We represent it as a n^2 length array.
+- We represent the matrix here as an array of length n^2, because that turned
+  out to be convenient for tic tac toe.
 
-there are 7 symmetries:
+There are 7 symmetries in a square matrix:
 - 90 degree rotation (cols reversed become rows)
 - 180 degree rotation (reverse array)
 - 270 degree rotation
@@ -13,9 +14,13 @@ there are 7 symmetries:
 - reflection across the horizontal (swap slices)
 - reflection across the vertical (reverse slices)
 
-symmetric boards are strategically equivalent.
+In tic tac toes, symmetric boards are strategically equivalent. So breaking these symmetries is interesting from an efficiency point of view.
 
 =end
+
+# ----------------------------------------------------------------------------
+# SYMMETRIES IN A SQUARE MATRIX
+# ----------------------------------------------------------------------------
 
 LENGTH = 3
 
@@ -54,10 +59,11 @@ def reflect_vertical(state)
   state.each_slice(LENGTH).flat_map(&:reverse)
 end
 
-# tests and benchmarks
+# ----------------------------------------------------------------------------
+# TESTS
+# ----------------------------------------------------------------------------
 
 state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-
 p rotate90(state) == [6, 3, 0, 7, 4, 1, 8, 5, 2] # true
 p rotate180(state) == [8, 7, 6, 5, 4, 3, 2, 1, 0] # true
 p rotate270(state) == [2, 5, 8, 1, 4, 7, 0, 3, 6] # true
@@ -66,12 +72,16 @@ p reflect_sec_diag(state) == [8, 5, 2, 7, 4, 1, 6, 3, 0] # true
 p reflect_horizontal(state) == [6, 7, 8, 3, 4, 5, 0, 1, 2] # true
 p reflect_vertical(state) == [2, 1, 0, 5, 4, 3, 8, 7, 6] # true
 
+# ----------------------------------------------------------------------------
+# BENCHMARKS
+# ----------------------------------------------------------------------------
+
 Benchmark.bmbm do |x|
   x.report("rot90") { rotate90(state) }
-  x.report("rot180") { rotate180(state) } # fast!
-  x.report("rot270") { rotate270(state) } # slow!
+  x.report("rot180") { rotate180(state) }
+  x.report("rot270") { rotate270(state) }
   x.report("trans") { transpose(state) }
-  x.report("sec_dg") { reflect_sec_diag(state) } # slow!
+  x.report("sec_dg") { reflect_sec_diag(state) }
   x.report("horiz") { reflect_horizontal(state) }
   x.report("vert") { reflect_vertical(state) }
 end
