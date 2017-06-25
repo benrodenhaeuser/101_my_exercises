93c53b03298c32cf267c0949c16614d476ae4644
Ben Rodenhäuser <ben@karo.berlin>
2017-06-25 23:37:43 +0200

    work on bridge crossing problem

--- backtracking/bridge_crossing.rb
+++ backtracking/bridge_crossing.rb
@@ -8,40 +8,11 @@ problem:
 - The soldiers have different walking speeds. In fact, we are going to assume that the speeds to cross the bridge are pairwise different.
 - Determine the minimum time needed to traverse the bridge, given walking times (in minutes) for all the soldiers.
 
-modeling:
-
-- input: an array `soldiers` (representing the number of soldiers (array size) and the traversal time for each soldier, i.e., `soldier[0]` is the traversal time for the first soldier, `soldier[1]` is the traversal time for the second soldier, and so on).
-- output: an integer (representing the minimal number of minutes needed to get everyone to the north of the bridge).
-
-observations:
-
-- we can solve this with depth-first search/backtracking.
-
-- what do we need?
-  - game position:
-    - current position of the soldiers
-    - (current position of the torch)
-    - time passed
-  - available moves in a game position (be careful to exclude infinite runs!)
-  - make/unmake function (i.e., making/unmaking a move)
-  - characterization of terminal position (i.e., everyone is in the south)
-
-  - a recursive function that computes the minimal time needed to reach
-    terminal position by searching the game tree.
-
-- what about *infinite runs*?
-  - it's very easy to produce infinite runs, for example, we could have one
-    particular soldier going back and forth with the torch alone ad infinitum
-  - policy:
-    - if the torch is south, two soldiers cross
-    - if the torch is north, one soldier crosses
-  - "obviously" we cannot save time doing it differently.
-
 =end
 
 def initial_state(times)
   {
-    soldiers: (0...times.size)
+    soldiers: (0...times.size).to_a
     positions: (0...times.size).map { |index| :south  },
     speed: times,
     timer: 0,
@@ -49,33 +20,45 @@ def initial_state(times)
   }
 end
 
-# a move: one-element or two-element array (elements are soldiers, i.e., indices)
-
-def moves(state)
-  if state[:torch] == :north
-    state[:soldiers].select do |soldier|
-      state[:positions][soldier] == :north
-    end.map { |soldier| [soldier] }
-  else
-    # any two soldiers in the south (as two-elem array)
+def soldiers_at(location, state)
+  state[:soldiers].select do |soldier|
+    state[:positions][soldier] == location
   end
 end
 
-def move_soldiers(state, soldiers)
-  # flip the position of every soldier in array soldiers
-  # add time accordingly
+def moves(state)
+  if state[:torch] == :north
+    soldiers_at(:north, state).map { |soldier| [soldier] }
+  else
+    soldiers = soldiers_at(:south, state)
+    soldiers.product(soldiers).select do |soldier1, soldier2|
+      soldier1 != soldier2
+    end
+  end
 end
 
-def unmove_soldiers(move, soldiers)
-  # flip the position of every soldier in array soldiers
-  # subtract time accordingly
+def make(move, state)
+  move.each do |soldier|
+    flip(soldier, state)
+    # add time for soldier
+  end
+end
+
+def unmake(move, state)
+  move.each do |soldier|
+    flip(soldier, state)
+    # subtract time for soldier
+  end
 end
 
 def terminal?(state)
   state[:positions].all? { |position| position == :north }
 end
 
-def solve(times)
-  state = initial_state(times)
-  # TODO: the backtracking algorithm
+def solve(state)
+  # the backtracking algorithm
+end
+
+def start(times)
+  solve(initial_state([1, 2, 5, 10]))
 end
