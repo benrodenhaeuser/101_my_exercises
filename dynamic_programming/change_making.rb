b4042a3a7e4dc075436c2b2435d6d4e70bff27b0
Ben Rodenhäuser <ben@karo.berlin>
2017-06-27 23:56:50 +0200

    minor

--- dynamic_programming/change_making.rb
+++ dynamic_programming/change_making.rb
@@ -1,5 +1,7 @@
 # Give change for amount n using the minimum number of coins of given denominations d1, d2, ..., dm. You may assume that the given denominations will include a penny, i.e., a coin with value 1.
 
+# We are here looking to find not the minimum number of coins, but a list of coins which sums up to n which is minimal in length.
+
 DENOMINATIONS = [1, 3, 5, 4, 6, 9, 1, 3]
 
 # ----------------------------------------------------------------------------
@@ -45,5 +47,6 @@ def change_tabu(amount)
   return table[amount]
 end
 
+p change_tabu(9)  # [9]
 p change_tabu(12) # [9, 3]
 p change_tabu(20) # [9, 6, 5] => instantaneous
