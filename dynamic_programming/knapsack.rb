67c97573ba0ccce500cae5185c08ff32eb84a82a
Ben Rodenhäuser <ben@karo.berlin>
2017-06-29 01:31:02 +0200

    lcs dynamic programming solution

--- dynamic_programming/knapsack.rb
+++ dynamic_programming/knapsack.rb
@@ -1,34 +1,40 @@
 # Given n items of known weights w1, w2, ..., wn and values v1, v2, ..., vn and a knapsack of capacity W, find the maximum value you can obtain by putting a selection of items into the knapsack.
 
-# approach:
+# -----------------------------------------------------------------------------
+# background
+# -----------------------------------------------------------------------------
 
-# we are looking for a recurrence knapsack(capacity, index), where size is the size of the knapsack, and index is the index of the current item in some list.
+=begin
 
-# to make the algorithm easier to express, we put in a dummy item at the beginning of the item list, which weighs nothing and has no value.
+we are looking for a recurrence knapsack(capacity, index), where size is the size of the knapsack, and index is the index of the current item in some list.
 
-# two base cases:
-# (1) 0th item ==> value is 0
-# (2) no capacity ==> value is 0
+to make the algorithm easier to express, we put in a dummy item at the beginning of the item list, which weighs nothing and has no value.
 
-# recursive case:
-# we are considering a knapsack of some capacity > 0 and an item which is not
-# at the beginning of the list of items
-# there are two options:
-# (1) current item goes into the knapsack
-# (2) current item does not go into the knapsack
+two base cases:
+(1) 0th item ==> value is 0
+(2) no capacity ==> value is 0
 
-# in case (1), the optimal value we can achieve is
-#    knapsack(capacity, index - 1)
-# in case (2), the optimal value we can achieve is
-#    value_of(index) + knapsack(capacity - weight(index), index - 1)
-# so we take the maximum of those two values.
+recursive case:
+we are considering a knapsack of some capacity > 0 and an item which is not
+at the beginning of the list of items
+there are two options:
+(1) current item goes into the knapsack
+(2) current item does not go into the knapsack
 
+in case (1), the optimal value we can achieve is
+   knapsack(capacity, index - 1)
+in case (2), the optimal value we can achieve is
+   value_of(index) + knapsack(capacity - weight(index), index - 1)
+so we take the maximum of those two values.
 
-# brute force: would be to enumerate all subsets of items, select the ones that fit in the knapsack, and choose among this selection the subset with the highest value
+=end
 
 # ----------------------------------------------------------------------------
 # implementing the recurrence
 # ----------------------------------------------------------------------------
+
+# the implementation uses a (weight- and valueless) dummy item prepended to the
+# list of items.
 
 ITEMS = [[0, 0], [1500, 1], [2000, 3], [3000, 4], [2000, 1]]
 
@@ -40,7 +46,7 @@ def weight_of(index)
   ITEMS[index].last
 end
 
-def knapsack(capacity, index)
+def knapsack(capacity, index = ITEMS.size - 1)
 
   if capacity == 0 || index == 0
     0
@@ -57,9 +63,14 @@ def knapsack(capacity, index)
 
 end
 
-# call with capacity and last valid index of ITEMS
-p knapsack(4, 4) # 4000 (correct)
+# call the method passing a capacity
+p knapsack(4) # returned value: 4000 (correct)
 
 # ----------------------------------------------------------------------------
 # dynamic programming solution with tabulation
 # ----------------------------------------------------------------------------
+
+# instead of (top-down) recursive calls knapsack(capacity, index), we have to
+# build a table knapsack[capacity, item] bottom-up.
+
+# TODO
---
+++ dynamic_programming/longest_common_subsequence.rb
@@ -0,0 +1,114 @@
+# problem: given two strings s and s', find the longest common subsequence of s
+# and s' using dynamic programming.
+
+# -----------------------------------------------------------------------------
+# background
+# -----------------------------------------------------------------------------
+
+=begin
+
+definition: given a string s, a string s' is a subsequence of s if there is an
+order-embedding f from s-indices to s'-indices such that
+s[i] = x => s'[f(i)] = x.
+
+an order-embedding is a function f such that for any i, j in the domain of f:
+i <= j iff f(i) <= f(j)
+
+Note that order-embeddings are always injective. Notice also that subsequences
+are not the same as a substrings!
+
+Example:
+`bde` is a subsequence of `abcde`.
+
+=end
+
+# -----------------------------------------------------------------------------
+# finding a recurrence
+# -----------------------------------------------------------------------------
+
+=begin
+
+express lcs(s, s') in terms of subproblems:
+decompose the strings from the right.
+there are two options:
+- the last two chars match, or they don't.
+- in the first case:
+      lcs(s, s') = lcs(s[0..-2], s'[0..-2]) + s[-1]
+= in the second case: lcs(s, s') is the maxium of
+      [lcs(s[0..-2], s'), lcs(s, s'[0..-2])]
+so that's the recurrence.
+
+what's the base case? "if one of the strings is empty, then the lcs is empty."
+
+=end
+
+# -----------------------------------------------------------------------------
+# implementing the recurrence
+# -----------------------------------------------------------------------------
+
+def lcs(string, other_string)
+  if string.length == 0 || other_string.length == 0
+    ''
+  elsif string[-1] == other_string[-1]
+    lcs(string[0..-2], other_string[0..-2]) + string[-1]
+  else
+    [
+      lcs(string[0..-2], other_string),
+      lcs(string, other_string[0..-2])
+    ].max
+  end
+end
+
+# -----------------------------------------------------------------------------
+# dynamic programming solution
+# -----------------------------------------------------------------------------
+
+def lcs_dp(string, other_string)
+
+  # insert blank prefix
+  string = ' ' + string
+  other_string = ' ' + other_string
+
+  # initialize table
+  lcs = []
+  (0...string.length).each { |index| lcs[index] = [] }
+  (0...string.length).each { |index| lcs[index][0] = ' ' }
+  (0...other_string.length).each { |index| lcs[0][index] = ' ' }
+
+  # fill in rows one by one
+  (1...string.length).each do |index| # for each row
+    (1...other_string.length).each do |other_index| # for each column
+
+      lcs[index][other_index] =
+        if string[index] == other_string[other_index]
+          lcs[index - 1][other_index - 1] + string[index]
+        else
+          [lcs[index - 1][other_index], lcs[index][other_index - 1]].max_by { |string| string.length }
+        end
+
+    end
+  end
+
+  # remove blank prefix and return
+  lcs[string.size - 1][other_string.size - 1].slice(1..-1)
+
+end
+
+# -----------------------------------------------------------------------------
+# tests
+# -----------------------------------------------------------------------------
+
+# p lcs('', '') == ''
+# p lcs('', 'a') == ''
+# p lcs('a', 'b') == ''
+# p lcs('a', 'a') == 'a'
+# p lcs('abcxuuuux', 'ux') == 'ux'
+# p lcs('kabcde', 'bkadcde') == 'kacde'
+
+p lcs_dp('', '') == ''
+p lcs_dp('', 'a') == ''
+p lcs_dp('a', 'b') == ''
+p lcs_dp('a', 'a') == 'a'
+p lcs_dp('ab', 'ab') == 'ab'
+p lcs_dp('abcxuuuux', 'ux') == 'ux'
+p lcs_dp('kabcde', 'bkadcde') == 'kacde'
